7519d137ddc5a949b38e21aabf85e1444935a584
Kevin Ludwig <signing@valaphee.com>
2022-05-07 02:40:23 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2022, Valaphee.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.palantir.git-version") version "0.12.3"
+    kotlin("jvm") version "1.6.10"
+    `maven-publish`
+    signing
+}
+
+group = "com.valaphee"
+val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+val details = versionDetails()
+version = "${details.lastTag}.${details.commitDistance}"
+
+repositories { mavenCentral() }
+
+dependencies { testImplementation("org.junit.jupiter:junit-jupiter:5.8.2") }
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "16"
+        targetCompatibility = "16"
+    }
+
+    withType<KotlinCompile>().configureEach { kotlinOptions { jvmTarget = "16" } }
+
+    withType<Test> { useJUnitPlatform() }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            pom.apply {
+                name.set("Bit Twiddle")
+                description.set("")
+                url.set("https://valaphee.com")
+                scm {
+                    connection.set("https://github.com/valaphee/bit-twiddle.git")
+                    developerConnection.set("https://github.com/valaphee/bit-twiddle.git")
+                    url.set("https://github.com/valaphee/bit-twiddle")
+                }
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://raw.githubusercontent.com/valaphee/jackson-dataformat-nbt/master/LICENSE.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("valaphee")
+                        name.set("Valaphee")
+                        email.set("iam@valaphee.com")
+                        roles.add("owner")
+                    }
+                }
+            }
+
+            from(components["java"])
+        }
+    }
+}
